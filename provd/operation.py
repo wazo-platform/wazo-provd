6dc36c8523063bb03f5f032dec0ef157e8eeaa18
Etienne Lessard <elessard@avencall.com>
2015-03-17 15:50:20 -0400

    remove some operation code
    Has been moved outside this repository.

--- provd/operation.py
+++ provd/operation.py
@@ -15,8 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-import re
-
 OIP_WAITING = 'waiting'
 OIP_PROGRESS = 'progress'
 OIP_SUCCESS = 'success'
@@ -84,53 +82,6 @@ def format_oip(oip):
     for sub_oip in oip.sub_oips:
         s += '(%s)' % format_oip(sub_oip)
     return s
-
-
-def _split_top_parentheses(str_):
-    idx = 0
-    length = len(str_)
-    result = []
-    while idx < length:
-        if str_[idx] != '(':
-            raise ValueError('invalid character: %s' % str_[idx])
-        start_idx = idx
-        idx += 1
-        count = 1
-        while count:
-            if idx >= length:
-                raise ValueError('unbalanced number of parentheses: %s' % str_)
-            c = str_[idx]
-            if c == '(':
-                count += 1
-            elif c == ')':
-                count -= 1
-            idx += 1
-        end_idx = idx
-        result.append(str_[start_idx + 1:end_idx - 1])
-    return result
-
-
-_PARSE_OIP_REGEX = re.compile(r'^(?:(\w+)\|)?(\w+)(?:;(\d+)(?:/(\d+))?)?')
-
-def parse_oip(oip_string):
-    """Return an operation in progress from a string.
-    
-    Valid strings are the same as the one returned by format_oip.
-    
-    Raise a ValueError if the string is invalid.
-    
-    """
-    m = _PARSE_OIP_REGEX.search(oip_string)
-    if not m:
-        raise ValueError('invalid oip string: %s' % oip_string)
-    else:
-        label, state, raw_current, raw_end = m.groups()
-        raw_sub_oips = oip_string[m.end():]
-        current = raw_current if raw_current is None else int(raw_current)
-        end = raw_end if raw_end is None else int(raw_end)
-        sub_oips = [parse_oip(sub_oip_string) for sub_oip_string in
-                    _split_top_parentheses(raw_sub_oips)]
-        return OperationInProgress(label, state, current, end, sub_oips)
 
 
 def operation_in_progres_from_deferred(deferred, label=None):
