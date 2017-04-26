c06f92ee938588cc9255774ba3dbfb05c6efbe8b
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-04-26 09:37:16 -0400

    update wazo email

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
 from setuptools import setup
 from setuptools import find_packages
@@ -9,7 +9,7 @@ setup(
     version='0.2',
     description='XiVO provisioning daemon',
     author='Wazo Authors',
-    author_email='dev.wazo@gmail.com',
+    author_email='dev@wazo.community',
     url='http://wazo.community',
     license='GPLv3',
 
