4e43ae3c08d7beb6bc7074ff0e86c2fc41babd49
Alexandre Fournier <afournier@wazo.io>
2018-11-21 10:18:36 -0500

    tests: add Plugin fixtures that work with operation in progress

--- integration_tests/suite/helpers/fixtures.py
+++ integration_tests/suite/helpers/fixtures.py
@@ -2,7 +2,13 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 
-class Device:
+from xivo_test_helpers import until
+from hamcrest import assert_that, is_
+
+from wazo_provd_client import operation
+
+
+class Device(object):
 
     def __init__(self, client, delete_on_exit=True):
         self._client = client
@@ -17,3 +23,31 @@ class Device:
     def __exit__(self, type, value, traceback):
         if self._delete_on_exit:
             self._client.devices.delete(self._device['id'])
+
+
+class Plugin(object):
+    def __init__(self, client, plugin_name, delete_on_exit=True):
+        self._client = client
+        self._plugin_name = plugin_name
+        self._plugin = None
+        self._delete_on_exit = delete_on_exit
+
+    def __enter__(self):
+        location = self._client.plugins.update()
+
+        until.assert_(operation_successful, self._client.plugins, location, tries=20, interval=0.5)
+
+        location = self._client.plugins.install(self._plugin_name)
+
+        until.assert_(operation_successful, self._client.plugins, location, tries=20, interval=0.5)
+        self._plugin = self._client.plugins.get(self._plugin_name)['plugin_info']
+        return self._plugin
+
+    def __exit__(self, type, value, traceback):
+        if self._delete_on_exit:
+            self._client.plugins.uninstall(self._plugin_name)
+
+
+def operation_successful(tested, location):
+    operation_progress = tested.get_operation(location)
+    assert_that(operation_progress.state, is_(operation.OIP_SUCCESS))
