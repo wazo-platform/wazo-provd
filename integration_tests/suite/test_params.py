9c8bcd00e9f3def2ada1fa3a5df1c3c6ab8d09e6
Alexandre Fournier <afournier@wazo.io>
2018-11-23 12:17:12 -0500

    tests: add provd params tests

---
+++ integration_tests/suite/test_params.py
@@ -0,0 +1,63 @@
+# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from hamcrest import (
+    assert_that,
+    has_key,
+    calling,
+    raises,
+)
+
+from xivo_test_helpers import until
+from wazo_provd_client import Client
+from wazo_provd_client.exceptions import ProvdError
+
+from .helpers.base import BaseIntegrationTest
+from .helpers.wait_strategy import NoWaitStrategy
+
+
+class TestParams(BaseIntegrationTest):
+
+    asset = 'base'
+    wait_strategy = NoWaitStrategy()
+
+    def setUp(self):
+        self._client = Client(
+            'localhost', https=False,
+            port=self.service_port(8666, 'provd'), prefix='/provd'
+        )
+
+    def tearDown(self):
+        pass
+
+    def test_get(self):
+        result = self._client.params.get('locale')
+        assert_that(result, has_key('param'))
+
+    def test_get_errors(self):
+        assert_that(
+            calling(self._client.params.get).with_args('invalid_param'),
+            raises(ProvdError)
+        )
+
+    def test_list(self):
+        result = self._client.params.list()
+        assert_that(result, has_key('params'))
+
+    def test_update(self):
+        self._client.params.update('locale', 'fr_FR')
+
+    def test_update_errors(self):
+        assert_that(
+            calling(self._client.params.update).with_args('invalid_param', 'invalid_value'),
+            raises(ProvdError)
+        )
+
+    def test_delete(self):
+        self._client.params.delete('locale')
+
+    def test_delete_errors(self):
+        assert_that(
+            calling(self._client.params.delete).with_args('invalid_param'),
+            raises(ProvdError)
+        )
