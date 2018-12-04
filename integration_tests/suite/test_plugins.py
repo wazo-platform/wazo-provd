5ccdbce8cc22bb10ec0d141335fe9e535a7e523c
Alexandre Fournier <afournier@wazo.io>
2018-12-04 13:34:33 -0500

    tests: plugins authentication tests

--- integration_tests/suite/test_plugins.py
+++ integration_tests/suite/test_plugins.py
@@ -56,6 +56,13 @@ class TestPlugins(BaseIntegrationTest):
             raises(ProvdError).matching(has_properties('status_code', 400))
         )
 
+    def test_install_error_when_invalid_token(self):
+        provd = self.make_provd('invalid-token')
+        assert_that(
+            calling(provd.plugins.install).with_args('invalid'),
+            raises(ProvdError).matching(has_properties('status_code', 401))
+        )
+
     def test_uninstall(self):
         with fixtures.Plugin(self._client, delete_on_exit=False):
             self._client.plugins.uninstall(PLUGIN_TO_INSTALL)
@@ -69,19 +76,47 @@ class TestPlugins(BaseIntegrationTest):
             raises(ProvdError).matching(has_properties('status_code', 400))
         )
 
+    def test_uninstall_error_when_invalid_token(self):
+        provd = self.make_provd('invalid-token')
+        assert_that(
+            calling(provd.plugins.uninstall).with_args('invalid_plugin'),
+            raises(ProvdError).matching(has_properties('status_code', 401))
+        )
+
     def test_list_installed(self):
         result = self._client.plugins.list_installed()
         assert_that(result, has_key('pkgs'))
 
+    def test_list_installed_error_invalid_token(self):
+        provd = self.make_provd('invalid-token')
+        assert_that(
+            calling(provd.plugins.list_installed),
+            raises(ProvdError).matching(has_properties('status_code', 401))
+        )
+
     def test_list_installable(self):
         result = self._client.plugins.list_installable()
         assert_that(result, has_key('pkgs'))
+
+    def test_list_installable_error_invalid_token(self):
+        provd = self.make_provd('invalid-token')
+        assert_that(
+            calling(provd.plugins.list_installable),
+            raises(ProvdError).matching(has_properties('status_code', 401))
+        )
 
     def test_update(self):
         with self._client.plugins.update() as operation_progress:
             until.assert_(
                 operation_successful, operation_progress, tries=10, timeout=10
             )
+
+    def test_update_error_invalid_token(self):
+        provd = self.make_provd('invalid-token')
+        assert_that(
+            calling(provd.plugins.update),
+            raises(ProvdError).matching(has_properties('status_code', 401))
+        )
 
     def test_get(self):
         with fixtures.Plugin(self._client) as result:
@@ -93,15 +128,39 @@ class TestPlugins(BaseIntegrationTest):
             raises(ProvdError).matching(has_properties('status_code', 404))
         )
 
+    def test_get_error_invalid_token(self):
+        provd = self.make_provd('invalid-token')
+        with fixtures.Plugin(self._client):
+            assert_that(
+                calling(provd.plugins.get).with_args(PLUGIN_TO_INSTALL),
+                raises(ProvdError).matching(has_properties('status_code', 401))
+            )
+
     def test_get_packages_installed(self):
         with fixtures.Plugin(self._client):
             result = self._client.plugins.get_packages_installed(PLUGIN_TO_INSTALL)
             assert_that(result, has_key('pkgs'))
 
+    def test_get_packages_installed_error_invalid_token(self):
+        provd = self.make_provd('invalid-token')
+        with fixtures.Plugin(self._client):
+            assert_that(
+                calling(provd.plugins.get_packages_installed).with_args(PLUGIN_TO_INSTALL),
+                raises(ProvdError).matching(has_properties('status_code', 401))
+            )
+
     def test_get_packages_installable(self):
         with fixtures.Plugin(self._client):
             result = self._client.plugins.get_packages_installable(PLUGIN_TO_INSTALL)
             assert_that(result, has_key('pkgs'))
+
+    def test_get_packages_installable_error_invalid_token(self):
+        provd = self.make_provd('invalid-token')
+        with fixtures.Plugin(self._client):
+            assert_that(
+                calling(provd.plugins.get_packages_installable).with_args(PLUGIN_TO_INSTALL),
+                raises(ProvdError).matching(has_properties('status_code', 401))
+            )
 
     def test_install_package(self):
         with fixtures.Plugin(self._client):
@@ -111,3 +170,11 @@ class TestPlugins(BaseIntegrationTest):
                     until.assert_(
                         operation_successful, progress, tries=10
                     )
+
+    def test_install_package_error_invalid_token(self):
+        provd = self.make_provd('invalid-token')
+        with fixtures.Plugin(self._client):
+            assert_that(
+                calling(provd.plugins.install_package).with_args(PLUGIN_TO_INSTALL, 'whatever'),
+                raises(ProvdError).matching(has_properties('status_code', 401))
+            )
