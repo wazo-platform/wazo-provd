2e73f98ed53ffd07bae33b15a35b3e4c9baad683
Alexandre Fournier <afournier@wazo.io>
2019-08-07 17:42:30 -0400

    security: rename fail2ban log file to wazo-provd-fail2ban.log

--- provd/security.py
+++ provd/security.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Avencall
+# Copyright 2016-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import logging
@@ -9,7 +9,7 @@ _logger = logging.getLogger(__name__)
 
 def setup_logging():
     formatter = logging.Formatter('[%(asctime)s] %(message)s')
-    handler = logging.FileHandler('/var/log/xivo-provd-fail2ban.log')
+    handler = logging.FileHandler('/var/log/wazo-provd-fail2ban.log')
     handler.setFormatter(formatter)
     _logger.addHandler(handler)
 
