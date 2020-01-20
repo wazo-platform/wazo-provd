1596d822f05988c8f9e7ebe7cacbe064ab43b70e
Pascal Cadotte Michaud <pcm@wazo.io>
2020-01-20 11:28:21 -0500

    http: add /status

---
+++ integration_tests/suite/test_status.py
@@ -0,0 +1,19 @@
+# Copyright 2020 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from hamcrest import (
+    assert_that,
+    has_entries,
+)
+
+from .helpers.base import BaseIntegrationTest
+from .helpers.wait_strategy import NoWaitStrategy
+
+
+class TestConfigs(BaseIntegrationTest):
+    asset = 'base'
+    wait_strategy = NoWaitStrategy()
+
+    def test_list(self):
+        results = self._client.status.get()
+        assert_that(results, has_entries(rest_api='ok'))
