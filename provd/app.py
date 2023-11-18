300c65316ef3b6ca1c0ea54265a5a96bb019f1ac
Francois Blackburn <fblackburn@wazo.community>
2023-11-18 14:41:58 -0500

    support provisioning_key without base_url defined
    why: by default, http_base_url is not defined. Fallbacking to ip:port is
    already the solution implemented in device plugins

--- provd/app.py
+++ provd/app.py
@@ -314,10 +314,16 @@ class ProvisioningApplication:
             provisioning_key = self.configure_service.get(
                 'provisioning_key', tenant_uuid
             )
+
             raw_config = copy.deepcopy(raw_config)
+            http_base_url = raw_config.get('http_base_url')
+            if not http_base_url:
+                host = raw_config['ip']
+                port = raw_config['http_port']
+                http_base_url = f'http://{host}:{port}'
             # Inject the provisioning key into the device configuration
-            http_base_url = raw_config['http_base_url']
             raw_config['http_base_url'] = f'{http_base_url}/{provisioning_key}'
+
         try:
             _check_raw_config_validity(raw_config)
         except Exception:
