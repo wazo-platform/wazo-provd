399adfa4012481c63ba11632fa417873b2a74bf5
Alexandre Fournier <afournier@wazo.io>
2023-12-08 17:01:43 -0500

    WIP add empty database

---
+++ integration_tests/suite/test_db.py
@@ -0,0 +1,13 @@
+# Copyright 2024 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+from .helpers.base import DBIntegrationTest
+from .helpers.wait_strategy import EverythingOkWaitStrategy
+
+
+class TestDatabaseIsUp(DBIntegrationTest):
+    asset = 'database'
+    wait_strategy = EverythingOkWaitStrategy
+
+    def test_that_database_launches(self) -> None:
+        self.start_postgres_service()
+        self.stop_postgres_service()
