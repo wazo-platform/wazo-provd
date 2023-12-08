2835aa31ce267a56672b50fa1ba2c23a3307d1a5
Alexandre Fournier <afournier@wazo.io>
2023-12-08 17:01:43 -0500

    WIP add empty database

---
+++ provd/database/alembic/versions/8763295f8c44_initial_revision.py
@@ -0,0 +1,23 @@
+"""initial revision
+
+Revision ID: 8763295f8c44
+Revises:
+Create Date: 2024-01-09 11:40:11.714942
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '8763295f8c44'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ provd/upgrade_db.py
@@ -0,0 +1,30 @@
+import logging
+import os
+
+import alembic.command
+import alembic.config
+from sqlalchemy import create_engine
+
+import provd.config
+from provd.database.helpers import wait_is_ready
+
+logger = logging.getLogger(__name__)
+
+
+def upgrade(uri: str) -> None:
+    current_dir = os.path.dirname(__file__)
+    config = alembic.config.Config(f'{current_dir}/alembic.ini')
+    config.set_main_option('script_location', f'{current_dir}/alembic')
+    config.set_main_option('sqlalchemy.url', uri)
+    config.set_main_option('configure_logging', 'false')
+
+    logger.info('Upgrading database')
+    engine = create_engine(uri)
+    wait_is_ready(engine)
+    alembic.command.upgrade(config, 'head')
+    logger.info('Database upgraded')
+
+
+def upgrade_db() -> None:
+    conf = provd.config.get_config(provd.config.Options())
+    upgrade(conf['database']['uri'])
