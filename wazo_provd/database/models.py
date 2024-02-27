76e6403de7c90515e3bfdc875a197f62ac86b6da
Alexandre Fournier <afournier@wazo.io>
2024-02-27 16:13:13 -0500

    models: add instance method as_dict
    Why: annoying to have to delete the _meta key and to have to
    import the dataclasses module only for this.

--- wazo_provd/database/models.py
+++ wazo_provd/database/models.py
@@ -6,10 +6,9 @@ from __future__ import annotations
 import abc
 import dataclasses
 import logging
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
-    from typing import Any
     from uuid import UUID
 
 logger = logging.getLogger(__name__)
@@ -18,6 +17,11 @@ logger = logging.getLogger(__name__)
 @dataclasses.dataclass
 class Model(metaclass=abc.ABCMeta):
     _meta: dict[str, Any]
+
+    def as_dict(self) -> dict[str, Any]:
+        self_dict = dataclasses.asdict(self)
+        del self_dict['_meta']
+        return self_dict
 
 
 @dataclasses.dataclass
