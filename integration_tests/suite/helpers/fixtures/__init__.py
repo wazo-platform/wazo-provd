4520a6816a897c924a0bb4c0a1dd84b97b547a18
Francois Blackburn <fblackburn@wazo.community>
2024-03-01 12:06:30 -0500

    test: add tenant_dao tests

--- integration_tests/suite/helpers/fixtures/__init__.py
+++ integration_tests/suite/helpers/fixtures/__init__.py
@@ -1,6 +1,6 @@
 # Copyright 2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from . import http
+from . import db, http
 
-__all__ = ['http']
+__all__ = ['db', 'http']
