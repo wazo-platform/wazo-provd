a1748c7cda69554c26ff3d0bdc1d9319ca78bd54
Alexandre Fournier <afournier@wazo.io>
2024-03-06 11:19:05 -0500

    fix typing issues

--- wazo_provd/config.py
+++ wazo_provd/config.py
@@ -167,6 +167,7 @@ class ProvdConfigDict(TypedDict):
     bus: BusConfigDict
     plugin_config: dict[str, Any]
     tenants: dict[str, dict]
+    proxy: dict[str, str]
 
 
 logger = logging.getLogger(__name__)
@@ -240,6 +241,7 @@ _DEFAULT_CONFIG: ProvdConfigDict = {
         'exchange_type': 'headers',
     },
     'plugin_config': {},
+    'proxy': {},
     'tenants': {},
 }
 
