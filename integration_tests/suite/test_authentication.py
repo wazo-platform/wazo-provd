3a70fb759a55fbc42451f23c293c2c4649d0fe82
Sebastien Duthil <sduthil@wazo.community>
2024-08-02 12:06:58 -0400

    return 401 code on missing token

---
+++ integration_tests/suite/test_authentication.py
@@ -0,0 +1,28 @@
+# Copyright 2024 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from hamcrest import assert_that, calling, has_properties
+from wazo_provd_client.exceptions import ProvdError
+from wazo_test_helpers.hamcrest.raises import raises
+
+from .helpers.base import BaseIntegrationTest
+
+
+class TestAuthentication(BaseIntegrationTest):
+    asset = 'base'
+
+    def test_no_token(self) -> None:
+        client = self.make_provd(None)
+
+        assert_that(
+            calling(client.status.get),
+            raises(ProvdError).matching(has_properties('status_code', 401)),
+        )
+
+    def test_invalid_token(self) -> None:
+        client = self.make_provd('invalid')
+
+        assert_that(
+            calling(client.status.get),
+            raises(ProvdError).matching(has_properties('status_code', 401)),
+        )
